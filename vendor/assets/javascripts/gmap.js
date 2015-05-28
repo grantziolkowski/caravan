1dae6af3a8e3951b0ec316496c0dc929dd0ecac7
Vinh Bui <bicyclethief@gmail.com>
2015-05-28 13:07:51 -0400

    Fix harvesting of google places info causing incorrect Trips matching

--- vendor/assets/javascripts/gmap.js
+++ vendor/assets/javascripts/gmap.js
@@ -1,76 +1,80 @@
 GmapAutoComplete = function(address) {
-  function getFieldIds() {
-    addressIds = {
-      field: address + "_address_string",
-      latitude: address + "_latitude",
-      longitude: address + "_longitude",
-      zipCode: address + "_zip_code",
-      city: address + "_city",
-      state: address + "_state",
-      streetAddress: address + "_street_address"
-    }
-    return addressIds
-  }
+  var addressFields = {
+    field: address + "_address_string",
+    latitude: address + "_latitude",
+    longitude: address + "_longitude",
+    zipCode: address + "_zip_code",
+    city: address + "_city",
+    state: address + "_state",
+    streetAddress: address + "_street_address"
+  };
+
+  var addressComponents = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'short_name',
+    postal_code: 'short_name'
+  };
+
   // Autocomplete
-  var addressInput = document.getElementById(getFieldIds().field);
-  var addressField = new google.maps.places.Autocomplete(addressInput);
-  var place;
-  var photos;
+  var options = {
+    types: ['geocode'],
+    componentRestrictions: {country: "us"}
+  };
+  var addressInput = document.getElementById(addressFields.field);
+  var addressField = new google.maps.places.Autocomplete(addressInput, options);
+
   // Autocomplete Listener
   google.maps.event.addListener(addressField, 'place_changed', function() {
-    place = addressField.getPlace();
+    var streetAddress, streetNumber, route, zipcode, city, state;
+    var place = addressField.getPlace();
 
-    document.getElementById(getFieldIds().latitude).value = place.geometry.location.lat();
-    document.getElementById(getFieldIds().longitude).value = place.geometry.location.lng();
+    // Get each component of the address from the place details
+    // and fill the corresponding field on the form.
+    for (var i = 0; i < place.address_components.length; i++) {
+      var addressType = place.address_components[i].types[0];
 
-    var address = '';
-    if (place.address_components) {
-      address = [(place.address_components[0] &&
-                  place.address_components[0].short_name || ''),
-                 (place.address_components[1] &&
-                  place.address_components[1].short_name || ''),
-                 (place.address_components[2] &&
-                  place.address_components[2].short_name || '')
-                ].join(' ');
+      if (addressComponents[addressType]) {
+        var val = place.address_components[i][addressComponents[addressType]];
+        switch(addressType) {
+          case "street_number":
+            streetNumber = val;
+            break;
+          case "route":
+            route = val;
+            break;
+          case "locality":
+            city = val;
+            break;
+          case "administrative_area_level_1":
+            state = val;
+            break;
+          case "postal_code":
+            zipCode = val;
+            break;
+        }
+      }
     }
+
     // Autofill the fields in the form. TODO: improve parsing of the returned JSON data.
-    var address = place.address_components;
-    if (address[address.length - 1]) {
-      var zipcode = address[address.length - 1].long_name;
-    };
-    if (address[address.length - 2]) {
-      var country = address[address.length - 2].long_name;
-    };
-    if (address[address.length - 3]) {
-       var state = address[address.length - 3].short_name
-     };
-    if (address.length === 9 ) {
-      var city = address[address.length - 3].long_name;
-     };
-    if (address.length === 8 ) {
-      var city = address[address.length - 5].long_name;
-     };
-    if (address[0]) {
-      var streetnumber = address[0].long_name;
-    };
-    if (address[1]) {
-      var streetname = address[1].long_name;
-    };
-    if (place.name) {
-      var location_name = place.name;
+    document.getElementById(addressFields.latitude).value = place.geometry.location.lat();
+    document.getElementById(addressFields.longitude).value = place.geometry.location.lng();
+
+    if (zipCode) {
+      document.getElementById(addressFields.zipCode).value = zipCode;
     }
 
-    if (zipcode){
-      document.getElementById(getFieldIds().zipCode).value = zipcode;
-    }
     if (city) {
-      document.getElementById(getFieldIds().city).value = city;
+      document.getElementById(addressFields.city).value = city;
     }
+
     if (state) {
-      document.getElementById(getFieldIds().state).value = state;
+      document.getElementById(addressFields.state).value = state;
     }
-    if (location_name) {
-      document.getElementById(getFieldIds().streetAddress).value = location_name;
+
+    if (streetNumber && route) {
+      document.getElementById(addressFields.streetAddress).value = streetNumber + ' ' + route;
     }
   });
-}
+};
